1aff3e7f39921e741b9b495fc7035a2c81402f4d
pjshu <shushugo233@gmail.com>
2021-01-16 18:09:08 +0800

    multiboot2 修改linker.ld

--- src/boot.s
+++ src/boot.s
@@ -64,18 +64,6 @@ doesn't make sense to return from this function as the bootloader is gone.
 .global _start
 .type _start, @function
 _start:
-	/*
-	The bootloader has loaded us into 32-bit protected mode on a x86
-	machine. Interrupts are disabled. Paging is disabled. The processor
-	state is as defined in the multiboot standard. The kernel has full
-	control of the CPU. The kernel can only make use of hardware features
-	and any code it provides as part of itself. There's no printf
-	function, unless the kernel provides its own <stdio.h> header and a
-	printf implementation. There are no security restrictions, no
-	safeguards, no debugging mechanisms, only what the kernel provides
-	itself. It has absolute and complete power over the
-	machine.
-	*/
 
 	/*
 	To set up a stack, we set the esp register to point to the top of the
