063c2170d418e361d3e12add4c6b876fac161e3f
joshuap233 <shushugo233@gmail.com>
2022-02-22 21:47:58 +0800

    shell

--- userspace/lib.h
+++ userspace/lib.h
@@ -41,12 +41,4 @@ bool strcmp(const char *s1, const char *s2);
 #endif // TEST
 
 
-struct dirent {
-    ino_t          d_ino;       /* Inode number */
-    off_t          d_off;       /* Not an offset; see below */
-    unsigned short d_reclen;    /* Length of this record */
-    unsigned char  d_type;      /* Type of file; not supported by all filesystem types */
-    char           d_name[256]; /* Null-terminated filename */
-};
-
 #endif //QUARKOS_USERSPACE_LIB_H
